be813b27c0eccc85513a99016f1e0b4745f1213b
positivevaib <vaib.gadodia@gmail.com>
2018-11-01 23:15:23 -0400

    Add files

---
+++ data.py
@@ -0,0 +1,12 @@
+# import packages
+import torch
+import torchvision
+import os
+
+# load training data
+training_data_original = torchvision.datasets.ImageFolder(root = 'Final_Training/Images', transform = torchvision.transforms.ToTensor())
+training_data_imadjust = torchvision.datasets.ImageFolder(root = 'Final_Training/imadjust', transform = torchvision.transforms.ToTensor())
+training_data_histeq = torchvision.datasets.ImageFolder(root = 'Final_Training/histeq', transform = torchvision.transforms.ToTensor())
+training_data_adapthisteq = torchvision.datasets.ImageFolder(root = 'Final_Training/adapthisteq', transform = torchvision.transforms.ToTensor())
+
+# load test data
