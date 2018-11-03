a53fa4e3ea66bca7604bdd5440de41ae6f07850d
positivevaib <vaib.gadodia@gmail.com>
2018-11-03 01:03:14 -0400

    Add file, edit comments and change class names

---
+++ main.py
@@ -0,0 +1,6 @@
+# import dependencies
+import torch
+from data import training_data_original, training_data_imadjust, training_data_histeq, training_data_adapthisteq
+from deep-neural-nets import Net1, Net2
+
+# load data
