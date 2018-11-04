097f9cd73adf69c2bcac708d82945c6aee8d103e
positivevaib <vaib.gadodia@gmail.com>
2018-11-04 14:03:16 -0500

    Update dependency names for import

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # import dependencies
 import torch
-from data import training_data_original, training_data_imadjust, training_data_histeq, training_data_adapthisteq
-from deep-neural-nets import Net1, Net2
-
-# load data
+from data import preprocess_data, load_data
+from dnn import Net1, Net2
