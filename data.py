3440352d3e273fd32ada473c9f3f0494464e74d5
positivevaib <vaib.gadodia@gmail.com>
2018-11-04 22:21:02 -0500

    Overwrite images

--- data.py
+++ data.py
@@ -11,7 +11,6 @@ def preprocess_data():
     '''crop images and call matlab preprocessing script'''
     # crop and resize images
     print('resizing images')
-
     # training set
     for image_class in os.listdir('training_set/original'):
         if not image_class.startswith('.'):
@@ -27,6 +26,7 @@ def preprocess_data():
                     image = Image.open('training_set/original/' + image_class + '/' + image_name)
                     image = image.crop((x1, y1, x2, y2))
                     image = image.resize((48, 48), resample = Image.BILINEAR)
+                    image.save('training_set/original/' + image_class + '/' + image_name)
 
     # test set
     annotations = pd.read_csv('test_set/images/GT-final_test.test.csv', sep = ';')
@@ -41,10 +41,10 @@ def preprocess_data():
             image = Image.open('test_set/images/' + image_name)
             image = image.crop((x1, y1, x2, y2))
             image = image.resize((48, 48), resample = Image.BILINEAR)
+            image.save('test_set/images/' + image_name)
 
     # normalize images
     print('normalizing images')
-
     eng = matlab.engine.start_matlab()
     eng.preprocessing(nargout = 0)
     eng.exit()
@@ -53,7 +53,6 @@ def load_data():
     '''load data'''
     # create validation split
     print('creating validation set')
-
     os.mkdir('validation_set')
     for file in os.listdir('training_set'):
         if not file.startswith('.'):
@@ -67,7 +66,6 @@ def load_data():
 
     # load training data
     print('loading training data')
-
     training_data = {}
     for file in os.listdir('training_set'):
         if not file.startswith('.'):
@@ -75,7 +73,6 @@ def load_data():
 
     # load validation data
     print('loading validation data')
-
     validation_data = {}
     for file in os.listdir('validation_set'):
         if not file.startswith('.'):
@@ -83,12 +80,11 @@ def load_data():
 
     # load test data
     print('loading test data')
-    
     test_data = []
     for image_name in os.listdir('test_set/images'):
         if image_name.endswith('.ppm'):
             image = Image.open('test_set/images/' + image_name)
-            image = torchvision.transforms.ToTensor(image)
+            image = torchvision.transforms.ToTensor()(image)
             test_data.append(image)
 
     # return data
