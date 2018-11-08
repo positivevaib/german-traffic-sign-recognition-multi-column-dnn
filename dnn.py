Fix model - Removed softmax from last layer. This allowed the model to train.

dnn.py
@@ -26,7 +26,7 @@ class Net1(nn.Module):
         x = F.max_pool2d(F.tanh(self.conv3(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.tanh(self.fc1(x))
-        x = F.softmax(self.fc2(x))
+        x = self.fc2(x)
         return x
 
     def num_flat_features(self, x):
@@ -51,7 +51,7 @@ class Net2(nn.Module):
         x = F.max_pool2d(F.tanh(self.conv3(x)), 2)
         x = x.view(-1, self.num_flat_features(x))
         x = F.tanh(self.fc1(x))
-        x = F.softmax(self.fc2(x))
+        x = self.fc2(x)
         return x
 
     def num_flat_features(self, x):